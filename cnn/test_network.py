1494540960173d1e4d77cee3d1392b4b63c68f14
emilettele <Ww13091993>
2018-06-06 02:14:07 +0200

    Lenet.py: - Sigmoid instead of softmax (not normalized anymore) Test_network.py + Train_network: - 28th class: Noise Data_augmenter: - White patches (squares) over the image representing scratches (only used on noise creation atm)
    Added new model trained on First dataset (small dataset) + noise
    Added example of noise image

--- cnn/test_network.py
+++ cnn/test_network.py
@@ -9,34 +9,36 @@ import numpy as np
 import argparse
 import cv2
 
+total = 0
 char_map = {
-    "Alef": 0,
-    "Ayin": 1,
-    "Bet": 2,
-    "Dalet": 3,
-    "Gimel": 4,
-    "He": 5,
+    "Alef"  : 0,
+    "Ayin"  : 1,
+    "Bet"   : 2,
+    "Dalet" : 3,
+    "Gimel" : 4,
+    "He"    : 5,
     "Het": 6,
     "Kaf": 7,
     "Kaf-final": 8,
     "Lamed": 9,
     "Mem": 10,
     "Mem-medial": 11,
-    "Nun-final": 12,
-    "Nun-medial": 13,
-    "Pe": 14,
-    "Pe-final": 15,
-    "Qof": 16,
-    "Resh": 17,
-    "Samekh": 18,
-    "Shin": 19,
-    "Taw": 20,
-    "Tet": 21,
-    "Tsadi-final": 22,
-    "Tsadi-medial": 23,
-    "Waw": 24,
-    "Yod": 25,
-    "Zayin": 26,
+    "Noise": 12,
+    "Nun-final": 13,
+    "Nun-medial": 14,
+    "Pe": 15,
+    "Pe-final": 16,
+    "Qof": 17,
+    "Resh": 18,
+    "Samekh": 19,
+    "Shin": 20,
+    "Taw": 21,
+    "Tet": 22,
+    "Tsadi-final": 23,
+    "Tsadi-medial": 24,
+    "Waw": 25,
+    "Yod": 26,
+    "Zayin": 27,
 }
 
 # construct the argument parse and parse the arguments
@@ -69,6 +71,7 @@ results = {}
 for i in range(len(prediction)):
     label = sorted(char_map.keys())[i]
     results[prediction[i]] = [label]
+    total += prediction[i]
 
 for result in sorted(results.items(), reverse=True):
     print result
@@ -80,7 +83,8 @@ label = char_map.keys()[char_map.values().index(argmax)]
 proba = prediction[argmax]
 label = "{}: {:.2f}%".format(label, proba * 100)
 
-print label;
+print label
+print total
 
 # draw the label on the image
 # output = imutils.resize(orig, width=400)
--- cnn/train_network.py
+++ cnn/train_network.py
@@ -30,21 +30,22 @@ char_map = {
     "Lamed": 9,
     "Mem": 10,
     "Mem-medial": 11,
-    "Nun-final": 12,
-    "Nun-medial": 13,
-    "Pe": 14,
-    "Pe-final": 15,
-    "Qof": 16,
-    "Resh": 17,
-    "Samekh": 18,
-    "Shin": 19,
-    "Taw": 20,
-    "Tet": 21,
-    "Tsadi-final": 22,
-    "Tsadi-medial": 23,
-    "Waw": 24,
-    "Yod": 25,
-    "Zayin": 26,
+    "Noise": 12,
+    "Nun-final": 13,
+    "Nun-medial": 14,
+    "Pe": 15,
+    "Pe-final": 16,
+    "Qof": 17,
+    "Resh": 18,
+    "Samekh": 19,
+    "Shin": 20,
+    "Taw": 21,
+    "Tet": 22,
+    "Tsadi-final": 23,
+    "Tsadi-medial": 24,
+    "Waw": 25,
+    "Yod": 26,
+    "Zayin": 27,
 }
 
 print datetime.datetime.now().time()
@@ -77,10 +78,10 @@ for imagePath in imagePaths:
     label = imagePath.split(os.path.sep)[-2]
     allLabel.append(char_map[label])
 
-# (trainX, testX, trainY, testY) = train_test_split(imagePaths, allLabel, test_size=0.10, random_state=42)
+(trainX, testX, trainY, testY) = train_test_split(imagePaths, allLabel, test_size=0.10, random_state=42)
 
-trainX = imagePaths
-trainY = allLabel
+# trainX = imagePaths
+# trainY = allLabel
 
 # print len(trainY), len(testY)
 for i in range(1,90):
@@ -90,21 +91,21 @@ for i in range(1,90):
         print BS
         break
 
-# testData = []
-# testLabel = []
-# for imagePath in testX:
-#     # load the image, pre-process it, and store it in the data list
-#     image = cv2.imread(imagePath)
-#     image = cv2.resize(image, (28, 28))
-#     image = img_to_array(image)
-#     testData.append(image)
-#
-#     label = imagePath.split(os.path.sep)[-2]
-#     testLabel.append(char_map[label])
-#
-# testData = np.array(testData, dtype="float") / 255.0
-# testLabel = np.array(testLabel)
-# testLabel = to_categorical(testLabel, num_classes=27)
+testData = []
+testLabel = []
+for imagePath in testX:
+    # load the image, pre-process it, and store it in the data list
+    image = cv2.imread(imagePath)
+    image = cv2.resize(image, (28, 28))
+    image = img_to_array(image)
+    testData.append(image)
+
+    label = imagePath.split(os.path.sep)[-2]
+    testLabel.append(char_map[label])
+
+testData = np.array(testData, dtype="float") / 255.0
+testLabel = np.array(testLabel)
+testLabel = to_categorical(testLabel, num_classes=28)
 
 
 def get_train_data(STEP):
@@ -127,13 +128,13 @@ def get_train_label(STEP):
         trainLabel.append(trainY[i])
 
     trainLabel = np.array(trainLabel)
-    trainLabel = to_categorical(trainLabel, num_classes=27)
+    trainLabel = to_categorical(trainLabel, num_classes=28)
 
     return trainLabel
 
 # initialize the model
 print("[INFO] compiling model...")
-model = LeNet.build(width=28, height=28, depth=3, classes=27)
+model = LeNet.build(width=28, height=28, depth=3, classes=28)
 opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
 model.compile(loss="binary_crossentropy", optimizer=opt,
               metrics=["accuracy"])
@@ -142,10 +143,10 @@ model.compile(loss="binary_crossentropy", optimizer=opt,
 print("[INFO] training network...")
 
 for i in range(len(trainX)/BS):
-    # model.fit(get_train_data(i), get_train_label(i), validation_data=(testData, testLabel), initial_epoch=i,
-    #           epochs=i+1, verbose=1)
-    model.fit(get_train_data(i), get_train_label(i), initial_epoch=i,
-              epochs=i + 1, verbose=1)
+    model.fit(get_train_data(i), get_train_label(i), validation_data=(testData, testLabel), initial_epoch=i,
+              epochs=5, verbose=1)
+    # model.fit(get_train_data(i), get_train_label(i), initial_epoch=i,
+    #           epochs=i + 1, verbose=1)
 
 # save the model to disk
 print("[INFO] serializing network...")
