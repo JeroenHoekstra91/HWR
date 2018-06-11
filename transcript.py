ce9d49027e82b752308aa2fc29a5e834ea1667d4
emilettele <Ww13091993>
2018-06-11 23:19:02 +0200

    cnn.py: Class that is able to test the windows for character recognition
    transcript.py:
    Indentation, coupled to CNN class, outputs file with results

--- transcript.py
+++ transcript.py
@@ -1,37 +1,48 @@
 import numpy as np
 import cv2
+from cnn.cnn import CNN
 
-cnn = None
 transcript = []
+cnn = CNN('cnn/ModelTrainedOnAugmentedData.model')
+file = open("output.txt", "w")
 
 def analyze_word_segment(word_segment, window_size=28, step_size=1, visualize=False):
-	segment_height = len(word_segment)
-	segment_width = len(word_segment[0])
-	
-	window_size = np.min((window_size, segment_width, segment_height))
-	window_x = 0
-	window_y = 0
+    segment_height = len(word_segment)
+    segment_width = len(word_segment[0])
 
-	x_iterations = int(np.ceil((segment_width - window_size) / (step_size * 1.0)))
-	y_iterations = int(np.ceil((segment_height - window_size) / (step_size * 1.0)))
+    window_size = np.min((window_size, segment_width, segment_height))
+    window_x = 0
+    window_y = 0
 
-	confidence_map = np.zeros_like(word_segment)
-	
-	# Slide window and analyze the character
-	for i in range(y_iterations):
-		# transcript[i] = ""
-		for j in range(x_iterations):
-			window = word_segment[window_y:(window_size+window_y), window_x:(window_size+window_x)]
-			analyze_character(window, visualize=visualize)
-			window_x += step_size
-		window_x = 0
-		window_y += step_size
+    x_iterations = int(np.ceil((segment_width - window_size) / (step_size * 1.0)))
+    y_iterations = int(np.ceil((segment_height - window_size) / (step_size * 1.0)))
 
-	if visualize:
-		cv2.destroyAllWindows()
+    confidence_map = np.zeros_like(word_segment)
+    file = open("output.txt", "a")
+
+    # Slide window and analyze the character
+    for i in range(y_iterations):
+        # transcript[i] = ""
+        for j in range(x_iterations):
+            window = word_segment[window_y:(window_size + window_y), window_x:(window_size + window_x)]
+            analyze_character(window, visualize=visualize)
+            window_x += step_size
+        window_x = 0
+        window_y += step_size
+
+    file.close()
+
+    if visualize:
+        cv2.destroyAllWindows()
+
 
 def analyze_character(window, visualize=False):
-	if visualize:
-		cv2.imshow("Test", window)
-		cv2.waitKey(100)
-	# Do something interesting with the character image
+    if visualize:
+        cv2.imshow("Test", window)
+        cv2.waitKey(100)
+    else:
+        total, results = cnn.analyze_character(window)
+        results = sorted(results.items(), reverse=True)
+        file.write(str(total)+str(results)+"\n")
+
+# Do something interesting with the character image
