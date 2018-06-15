6ca82c49dd1bffb4bec9f3bd42ee1785a0df8919
JeroenHoekstra <jeroenhoekstra_1991@hotmail.com>
2018-06-15 13:04:57 +0200

    Fixes draw extremes method

--- transcript.py
+++ transcript.py
@@ -84,18 +84,20 @@ def print_character_confidence(extremes, confidence_map, character_map):
 		print "\tavg: " + str(np.average(d[key]))
 		print "\tmin: " + str(np.min(d[key]))
 		print "\tmax: " + str(np.max(d[key]))
+		print "\tcount: " + str(len(d[key]))
 
 def draw_extremes(extremes, image, confidence_map, character_map, out="extremes/", window_size=50, step_size=1):
-	font = cv2.FONT_HERSHEY_SIMPLEX
-	label = character_map[coor[0]][coor[1]]
-	image_extreme = image.copy()
-	image_coor = map_coordinate_to_image_coordinate(coor[0], coor[1],
-		window_size=window_size, step_size=step_size)
-	filename = out + label + "(" + str(image_coor[1]) + ", " + str(image_coor[0]) + ").png"
+	for coor in extremes:
+		font = cv2.FONT_HERSHEY_SIMPLEX
+		label = character_map[coor[0]][coor[1]]
+		image_extreme = image.copy()
+		image_coor = map_coordinate_to_image_coordinate(coor[0], coor[1],
+			window_size=window_size, step_size=step_size)
+		filename = out + label + "(" + str(image_coor[1]) + ", " + str(image_coor[0]) + ").png"
 
-	xx = int(image_coor[1] - window_size/2.0)
-	yy = int(image_coor[0] - window_size/2.0)
-	
-	cv2.rectangle(image_extreme,(xx,yy),(xx+window_size,yy+window_size),(0,0,255),1)
-	cv2.putText(image_extreme, label, (1,len(image)-5), font, .5,(0,0,255),1,cv2.LINE_AA)
-	cv2.imwrite(filename, image_extreme)
+		xx = int(image_coor[1] - window_size/2.0)
+		yy = int(image_coor[0] - window_size/2.0)
+		
+		cv2.rectangle(image_extreme,(xx,yy),(xx+window_size,yy+window_size),(0,0,255),1)
+		cv2.putText(image_extreme, label, (1,len(image)-5), font, .5,(0,0,255),1,cv2.LINE_AA)
+		cv2.imwrite(filename, image_extreme)
