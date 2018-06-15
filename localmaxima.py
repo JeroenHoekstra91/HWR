38a9bb0d6049251473dcc538cfae2484f00fc966
JeroenHoekstra <jeroenhoekstra_1991@hotmail.com>
2018-06-15 12:02:44 +0200

    Filters candidate windows

--- localmaxima.py
+++ localmaxima.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.signal import savgol_filter
 
 """
 creates numpy grid with a two dimensional hyperbola (kind of a cone)
@@ -13,15 +14,16 @@ array([[ 3.6,  5. ,  6. ,  6.6,  6.8,  6.6,  6. ,  5. ,  3.6],
        [ 5. ,  6.4,  7.4,  8. ,  8.2,  8. ,  7.4,  6.4,  5. ],
        [ 3.6,  5. ,  6. ,  6.6,  6.8,  6.6,  6. ,  5. ,  3.6]])
 """
-def create_grid(center=5, maximum=10):
+def create_grid(center=5, maximum=10, noise_level=0):
 	matrix = np.zeros((9, 9))
 	for i in range(9):
 		for j in range(9):
 			yy = j + 1
 			xx = i + 1
 
+			noise = np.random.random(1)[0] * noise_level
 			distance = pow(pow(xx-center,2) + pow(yy-center,2) , .5)
-			matrix[j, i] = maximum - pow(distance, 2) * .2
+			matrix[j, i] = (maximum - pow(distance, 2) * .2) + noise
 	return matrix
 
 def compute_gradient(matrix):
@@ -29,6 +31,13 @@ def compute_gradient(matrix):
 	dy = np.abs(nabla[0])
 	dx = np.abs(nabla[1])
 	return dx + dy
+
+def smooth(matrix, rounds=5):
+	smoothed = matrix.copy()
+	for _ in range(rounds):
+		smoothed = savgol_filter(smoothed,5,2)
+		smoothed = savgol_filter(smoothed,5,2, axis=0)
+	return smoothed
 
 def get_local_extrema(matrix, threshold=0, peak_estimation_threshold=0.001):
 	gradient = compute_gradient(matrix)
--- transcript.py
+++ transcript.py
@@ -1,11 +1,12 @@
 import numpy as np
 import cv2
 from cnn.cnn import CNN
+from localmaxima import *
 
 cnn = CNN('cnn/ModelTrainedWithNoise.model')
 topN = 3
 
-def analyze_word_segment(word_segment, window_size=28, step_size=1, visualize=False):
+def analyze_word_segment(word_segment, window_size=50, step_size=1, visualize=False):
 	segment_width = len(word_segment[0])
 	segment_height = len(word_segment)
 
@@ -13,11 +14,11 @@ def analyze_word_segment(word_segment, window_size=28, step_size=1, visualize=Fa
 	window_x = 0
 	window_y = 0
 
-	x_iterations = int(np.ceil((segment_width - window_size) / (step_size * 1.0)))
-	y_iterations = int(np.ceil((segment_height - window_size) / (step_size * 1.0)))
+	x_iterations = int(np.ceil((segment_width - window_size) / (step_size * 1.0))) + 1
+	y_iterations = int(np.ceil((segment_height - window_size) / (step_size * 1.0))) + 1
 
-	map_width = int(np.ceil((segment_width - (window_size * 1.0)) / step_size))
-	map_height = int(np.ceil((segment_height - (window_size * 1.0)) / step_size))
+	map_width = x_iterations
+	map_height = y_iterations
 	confidence_map = [np.zeros((map_height, map_width)) for _ in range(topN)]
 	character_map = [[['' for _ in range (map_width)] for _ in range(map_height)] for _ in range(topN)]
 
@@ -42,7 +43,6 @@ def analyze_word_segment(word_segment, window_size=28, step_size=1, visualize=Fa
 
 	return confidence_map, character_map
 
-
 def analyze_character(window, visualize=False):
 	if visualize:
 		cv2.imshow("Test", window)
@@ -52,3 +52,50 @@ def analyze_character(window, visualize=False):
 	results = sorted(results.items(), reverse=True)[:topN]
 	# perhaps threshold on total score?
 	return results
+
+def map_coordinate_to_image_coordinate(x, y, window_size=50, step_size=1):
+	xx = x*step_size + window_size/2.0
+	yy = y*step_size + window_size/2.0
+	return xx, yy
+
+def filter_extremes(extremes, character_map):
+	filtered = list(extremes)
+	for coor in extremes:
+		# filter noise extremes
+		if character_map[coor[0]][coor[1]] == "Noise":
+			filtered.remove(coor)
+	return filtered
+
+def get_character_confidence(extremes, confidence_map, character_map):
+	character_confidence = {}
+	for coor in extremes:
+		char = character_map[coor[0]][coor[1]]
+		confidence = confidence_map[coor[0], coor[1]]
+		if char in character_confidence.keys():
+			character_confidence[char].append(confidence)
+		else:
+			character_confidence[char] = [confidence]
+	return character_confidence
+
+def print_character_confidence(extremes, confidence_map, character_map):
+	d = get_character_confidence(extremes, confidence_map, character_map)
+	for key in d.keys():
+		print key + "]"
+		print "\tavg: " + str(np.average(d[key]))
+		print "\tmin: " + str(np.min(d[key]))
+		print "\tmax: " + str(np.max(d[key]))
+
+def draw_extremes(extremes, image, confidence_map, character_map, out="extremes/", window_size=50, step_size=1):
+	font = cv2.FONT_HERSHEY_SIMPLEX
+	label = character_map[coor[0]][coor[1]]
+	image_extreme = image.copy()
+	image_coor = map_coordinate_to_image_coordinate(coor[0], coor[1],
+		window_size=window_size, step_size=step_size)
+	filename = out + label + "(" + str(image_coor[1]) + ", " + str(image_coor[0]) + ").png"
+
+	xx = int(image_coor[1] - window_size/2.0)
+	yy = int(image_coor[0] - window_size/2.0)
+	
+	cv2.rectangle(image_extreme,(xx,yy),(xx+window_size,yy+window_size),(0,0,255),1)
+	cv2.putText(image_extreme, label, (1,len(image)-5), font, .5,(0,0,255),1,cv2.LINE_AA)
+	cv2.imwrite(filename, image_extreme)
