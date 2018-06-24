d42befa63500dc51baba584ee2a16401a0c98a75
JeroenHoekstra <jeroenhoekstra_1991@hotmail.com>
2018-06-25 01:29:20 +0200

    Cleanup

--- util/transcript.py
+++ util/transcript.py
@@ -1,7 +1,7 @@
 import numpy as np
 import types
 
-from settings import txt_output_filename, Ngrams
+from settings import Ngrams
 from util.character_map import hebrew_map
 
 
@@ -48,12 +48,10 @@ def generate_transcripts(ngrams_model, sorted_window_groups, character_map, conf
     return transcripts
 
 
-def write_to_file(word):
-    fw = open(txt_output_filename + ".txt", "a")
-    for character in word.split(" "):
-        fw.write(hebrew_map[character])
-    fw.write("\n")
-    fw.close()
+def write_to_file(filename, word):
+    file = open(filename, "a")
+    file.write(to_hebrew(word) + "\n")
+    file.close()
 
 
 def to_hebrew(word):
@@ -64,8 +62,10 @@ def to_hebrew(word):
 
 
 def sort_by_relevance(transcripts, cnn_confidence_weight=1, ngrams_likelihood_weight=1):
-    transcripts[0].sort(key=lambda x: x["cnn_confidence_sum"] * cnn_confidence_weight + x[
-         "ngrams_likelihood"] * ngrams_likelihood_weight, reverse=True)
+    transcripts[0].sort(key=lambda x:
+        x["cnn_confidence_sum"] * cnn_confidence_weight + 
+        x["ngrams_likelihood"] * ngrams_likelihood_weight,
+        reverse=True)
 
 
 def filter_transcripts(transcripts, ngrams_likelihood_threshold=0.0):
@@ -105,7 +105,6 @@ def where(element, collection):
             index.append(0)
         else:
             index[-1] += 1
-
 
 #### HELPER FUNCTIONS ####
 
