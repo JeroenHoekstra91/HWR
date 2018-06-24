772f9a11e4762fbb6b369c007fc7a49c3a50e3b6
emilettele <emilettele@hotmail.com>
2018-06-24 21:06:35 +0200

    Indentation according to Python standards

--- util/image.py
+++ util/image.py
@@ -1,9 +1,10 @@
 import numpy as np
 import cv2
 
+
 def pad_image(image, padding=5, window_size=50, step_size=1):
-	top, bottom = 0, 0
-	if len(image) < window_size + step_size:
-		top = int(np.ceil((step_size + window_size - len(image)) / 2.0))
-		bottom = top
-	return cv2.copyMakeBorder(image, top, bottom, padding, padding, cv2.BORDER_CONSTANT, value=(255,255,255))
+    top, bottom = 0, 0
+    if len(image) < window_size + step_size:
+        top = int(np.ceil((step_size + window_size - len(image)) / 2.0))
+        bottom = top
+    return cv2.copyMakeBorder(image, top, bottom, padding, padding, cv2.BORDER_CONSTANT, value=(255, 255, 255))
