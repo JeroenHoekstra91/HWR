f8df44330ba591b05f224e2c66420b86151d9a25
JeroenHoekstra <jeroenhoekstra_1991@hotmail.com>
2018-06-24 17:25:31 +0200

    Fixes wordsegment shorter than window bug

--- util/image.py
+++ util/image.py
@@ -1,4 +1,9 @@
+import numpy as np
 import cv2
 
-def pad_image(image, padding=5):
-	return cv2.copyMakeBorder(image, 0, 0, padding, padding, cv2.BORDER_CONSTANT, value=(255,255,255))
+def pad_image(image, padding=5, window_size=50, step_size=1):
+	top, bottom = 0, 0
+	if len(image) < window_size + step_size:
+		top = int(np.ceil((step_size + window_size - len(image)) / 2.0))
+		bottom = top
+	return cv2.copyMakeBorder(image, top, bottom, padding, padding, cv2.BORDER_CONSTANT, value=(255,255,255))
